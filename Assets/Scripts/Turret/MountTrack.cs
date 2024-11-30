94d2ed4c7961d15ae515c658be3f014129d42218
haweom <100crocki@gmail.com>
2024-11-30 21:22:05 +0100

    Add flying turret

--- Assets/Scripts/Turret/MountTrack.cs
+++ Assets/Scripts/Turret/MountTrack.cs
@@ -1,14 +1,21 @@
+using System;
 using Interface;
 using UnityEngine;
 
 public class CannonTracking : MonoBehaviour
 {
     private Transform _playerTrans;
-    //[SerializeField] private float angleFix = 60f;
+    private float _angleFix;
+    [SerializeField] private bool mode;
     private void Awake()
     {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         _playerTrans = player.transform;
+    }
+
+    private void Start()
+    {
+        
     }
 
     private void Update()
@@ -20,7 +27,7 @@ public class CannonTracking : MonoBehaviour
     {
         Vector3 direction = _playerTrans.position - transform.position;
         
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 160;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - (mode ? 160f : 90f);
         
         transform.rotation = Quaternion.Euler(0f, 0f, angle);
     }
