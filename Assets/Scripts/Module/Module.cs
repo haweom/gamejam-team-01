d3c2e0f23037cc34d616a23037de44cae525c845
sol4ar <83139747+sol4ar@users.noreply.github.com>
2024-11-30 19:25:10 +0100

    Module Hit cooldown

--- Assets/Scripts/Module/Module.cs
+++ Assets/Scripts/Module/Module.cs
@@ -2,21 +2,32 @@ using UnityEngine;
 
 public class Module : MonoBehaviour
 {
-    public bool Usable { get; private set; } = true;
+    public bool Destroyed => Health > 0;
 
     public int MaxHealth;
     public int Health { get; private set; }
+
+    public float HitCooldown = 1.5f;
+    private float _hitCooldownTimer;
 
     void Start()
     {
         Health = MaxHealth;
     }
 
+    private void FixedUpdate()
+    {
+        if (_hitCooldownTimer > 0) _hitCooldownTimer -= Time.deltaTime;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.gameObject.CompareTag("Bullet")) return;
+        if (_hitCooldownTimer > 0) return;
 
         var bullet = collision.gameObject.GetComponent<BulletLaunch>();
         Health -= bullet.Damage;
+
+        _hitCooldownTimer = HitCooldown;
     }
 }
