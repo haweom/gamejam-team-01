3a24ad363dd7130e7d07ce937ef2c5f21248861e
haweom <100crocki@gmail.com>
2024-11-30 15:25:11 +0100

    Add ship turret functionality

--- Assets/Scripts/Ship/Ship Turret/ShipCannon.cs
+++ Assets/Scripts/Ship/Ship Turret/ShipCannon.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class ShipCannon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private float shootForce = 10f;
+    [SerializeField] private float shootInterval = 0.2f;
+    
+    private float _shootTimer;
+    private Transform _shootPoint;
+    
+    private void Awake()
     {
-        
+        _shootPoint = transform.Find("ShootPoint");
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private void Update()
     {
+        _shootTimer += Time.deltaTime;
         
+        if (Input.GetMouseButton(0) && _shootTimer >= shootInterval)
+        {
+            Shoot();
+            _shootTimer = 0f;
+        }
+    }
+    
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, _shootPoint.position, _shootPoint.rotation);
+        
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.AddForce(_shootPoint.up * shootForce, ForceMode2D.Impulse);
+        }
     }
 }
